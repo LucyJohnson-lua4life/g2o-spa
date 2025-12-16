a560c49516161ff76e011f42406024f27d59277d
Lucy Johnson <lucy.johnson.99.08.18@gmail.com>
2025-12-16 22:28:43 +0100

    chore: update and add first ingame page handler

--- src/app/ingame/ingame-page.ts
+++ src/app/ingame/ingame-page.ts
@@ -1,4 +1,4 @@
-import { Component, signal, NgZone } from '@angular/core';
+import { Component, signal, NgZone, ChangeDetectorRef } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -6,8 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { AppStateService } from '../app-state-service';
-import { sha256 } from '@noble/hashes/sha2.js';
-import { utf8ToBytes, bytesToHex } from '@noble/hashes/utils.js';
+
 declare const squirrel: any;
 
 @Component({
@@ -19,9 +18,38 @@ declare const squirrel: any;
 })
 export class IngamePage {
     username = signal('');
+    showTalkButton = signal(false);
 
-    constructor(private appState: AppStateService, private ngZone: NgZone) {
+    constructor(private appState: AppStateService, private ngZone: NgZone, private cd: ChangeDetectorRef) {
+        (window as any).displayTalkButton = this.displayTalkButton.bind(this);
+        (window as any).hideTalkButton = this.hideTalkButton.bind(this);
+    }
+    displayTalkButton() {
+        this.ngZone.run(() => {
 
+            // Show the talk button. External code can call window.displayTalkButton()
+            this.showTalkButton.set(true);
+            squirrel.call("cefLog", "display hook called");
+            this.cd.markForCheck();
+        });
     }
 
+    hideTalkButton() {
+        this.ngZone.run(() => {
+
+            // Hide the talk button.
+            this.showTalkButton.set(false);
+                        squirrel.call("cefLog", "hide hook called");
+            this.cd.markForCheck();
+        });
+    }
+
+    onTalkClick() {
+        // Handle the talk button click. You can customize behavior here.
+        // Hide the button after click to mimic dialog initiation.
+        this.showTalkButton.set(false);
+
+
+
+    }
 }
